413352bc02032df416f35fa7753e092f4f38dff1
Michael Macias <michael.maciasvidal@gmail.com>
2026-03-20 12:27:15 -0300

    Delete fix-structure-wizard-errors3.js

--- fix-structure-wizard-errors3.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const file = 'components/settings/StructureWizard.tsx';
-let content = fs.readFileSync(file, 'utf8');
-
-content = content.replace(/region: finalRegion,\n/g, '');
-content = content.replace(/micro_zone_id: microZoneId/g, ''); // Will not use since payload schema does not seem to have it
-content = content.replace(/const mzRes = await createMicroZone\(\{ site_id: "pending_override_or_leave_null", name: microZoneName\.trim\(\) \}\);\n        if \(\!mzRes\) throw new Error\("Failed to create micro-zone"\);\n        microZoneId = mzRes\.id;\n/g, '');
-
-fs.writeFileSync(file, content);
